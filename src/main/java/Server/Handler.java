34be2954d16dff7b3454146eb0bdc57082466a79
Artem Sudkov <shaotrader@gmail.com>
2023-10-19 21:51:29 +0500

    1st vers

---
+++ src/main/java/Server/Handler.java
@@ -0,0 +1,10 @@
+package Server;
+
+import com.sun.net.httpserver.Request;
+
+import java.io.BufferedOutputStream;
+
+@FunctionalInterface
+public interface Handler {
+    void handle(Request request, BufferedOutputStream outputStream);
+}
